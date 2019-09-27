43051b9e8ba4f20fa7900c5c10cc6f257c58ec1f
Andreu Martínez <andreu.ma.ma@hotmail.com>
2019-09-27 17:46:57 +0200

    Segon Commit

---
+++ A4/index1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php	
+		$cssFile= $_GET["skinfile"].".css";
+		echo "<link rel='stylesheet' type='text/css' href='css/".$cssFile."'>";
+		$dir = opendir("css");
+		$array=[];
+		$array[]=$arxiu = readdir($dir);
+		while($arxiu!=false){
+			$arxiu = readdir($dir);
+			if($arxiu!="."&&$arxiu!=".."&&$arxiu!=""){
+				$array[]=$arxiu;
+			}
+		}
+		$count=count($array);
+		closedir($dir);
+	?>	
+</head>
+<body>
+	<FORM>
+	<SELECT name="skinfile">
+		<?php
+			for($i=0;$i<$count;$i++){
+				echo "<option >".substr($array[$i],0,-4)."</option>";
+			}		
+		?>
+	</SELECT>
+ 	<BR />
+	<INPUT type="submit" />
+</FORM>
+</body>
+</html>
+
+
+
